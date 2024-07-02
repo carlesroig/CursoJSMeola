a36b9daf633dd9057f4288b88ee7099e9abee3b6
Marc Meola <mmeola@autis.com>
2024-07-02 18:00:30 +0200

    Cursos

---
+++ Curso Personal/Vue/2 Seccion/basics-10-styling-starting-setup/app.js
@@ -0,0 +1,15 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            boxA:false,
+            boxB:false,
+            boxC:false,
+        }
+    },
+    method:{
+        boxselected(box){
+            if(box)
+        }
+    }
+});
+app.mount('#styling')
